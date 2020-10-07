ae2802701522308613f9a1bd7939cff3e83c8d13
PriscillaBuhrle <priscillabuhrlerocha@gmail.com>
2020-10-07 19:45:11 -0300

    Se termina requerimientos de bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,28 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  /*modal*/
+
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+  /*scroll*/
+  $(function(){
+
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
+  });
   
